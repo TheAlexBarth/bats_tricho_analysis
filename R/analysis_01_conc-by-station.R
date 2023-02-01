85b540a1fb2e9c75e5cdc79fb0d0b6e4ca60dba7
megan-eubanks <MME4@email.sc.edu>
2023-02-01 14:49:08 -0500

    intg casts with lapply

--- R/analysis_01_conc-by-station.R
+++ R/analysis_01_conc-by-station.R
@@ -14,11 +14,18 @@ name_map <- list(
                                   metadata$stationid == 'bloom')]
 )
 
-# Average casts by station
-
-
 # Integrate cast profiles
-  
+
+intg_casts <-tricho_conc |>
+    lapply(integrate_all, need_format= TRUE, subdivisions=1000) |>
+      lapply(intg_to_tib) |>
+        list_to_tib('profileid')
+
+
+# Average casts by station
+avg_tricho <- average_casts(name_map=name_map, zoo_conc_list = tricho_conc)
+
+
 # Create a plot for the average profile
 s
   

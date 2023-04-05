412e6235cba1859208e6d8916b71caa51835196b
megan-eubanks <MME4@email.sc.edu>
2023-04-05 14:34:56 -0400

    day/night figure

--- R/analysis_04_dvm.R
+++ R/analysis_04_dvm.R
@@ -19,6 +19,27 @@ tod_map <- list(
 # Average Casts by station
 
 avg_conc <- tricho_conc |> 
-  average_casts(name_map = station_map) |> 
+  average_casts(name_map = tod_map) |> 
   lapply(bin_format)
+ 
+#ggplot here 
 
+ggplot()+
+  geom_rect(data = avg_conc$night,
+            aes(xmin = min_d, xmax = max_d,
+                ymin = 0, ymax = -mean)) +
+  geom_errorbar(data = avg_conc$night,
+                aes(x = mp, ymin = -mean, ymax = -(mean+sd)),
+                alpha = .25) +
+  geom_rect(data = avg_conc$day,
+            aes(xmin = min_d, xmax = max_d,
+                ymin = 0, ymax = mean)) +
+  geom_errorbar(data = avg_conc$day,
+                aes(x = mp, ymin = mean, ymax = (mean+sd)),
+                alpha = .25)+
+  labs(x = 'Depth [m]', y = 'Concentration [colonies/m3]')+
+  coord_flip()+
+  scale_x_reverse(expand = c(0,0))+
+  scale_y_continuous(position = 'right',
+                     labels = abs) +
+    theme_bw() 
